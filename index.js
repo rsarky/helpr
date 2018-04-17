e2af6d2f454aecfb25795f07fea5b46a65049a06
Rohit Sarkar <rohitsarkar5398@gmail.com>
2018-04-18 00:51:33 +0530

    Multiple issues referenced in a PR body can now be handled thanks to issue-parser

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+const issueParser = require('issue-parser')
+
+const parse = issueParser('github')
+
 module.exports = (robot) => {
   // TODO: Comment on issue along with adding label on pull_request events.
   // TODO: pull_request.reopened?
   robot.on(['pull_request.opened',
-            'pull_request.edited',
-            'pull_request.closed'], pullHandler)
+    'pull_request.edited',
+    'pull_request.closed'], pullHandler)
 
   async function pullHandler (context) {
     const { github, payload } = context
@@ -11,50 +15,77 @@ module.exports = (robot) => {
     // Create the labels. TODO : Add a config file for labels and label colors.
     try {
       await Promise.all([
-        github.issues.createLabel(context.repo({name: 'pr-available', color: '1381ef'})),
-        github.issues.createLabel(context.repo({name: 'pr-merged', color: '1381ef'})),
-        github.issues.createLabel(context.repo({name: 'pr-rejected', color: '1381ef'}))
+        github.issues.createLabel(context.repo({ name: 'pr-available', color: '1381ef' })),
+        github.issues.createLabel(context.repo({ name: 'pr-merged', color: '1381ef' })),
+        github.issues.createLabel(context.repo({ name: 'pr-rejected', color: '1381ef' }))
       ])
-    } catch (e) {}
+    } catch (e) { }
 
-    const issueNum = parse(body)
-    if (issueNum) {
+    const issueNum = parser(body)
+    if (issueNum[0]) {
       context.log(`Issue ${issueNum} has been referenced.`)
 
-      // Add label to the issue.
       if (state === 'open') {
+
         // Remove pr-rejected label if it exists.
         try {
-          await github.issues.removeLabel(context.repo({number: issueNum, name: 'pr-rejected'}))
-        } catch (e) {}
-        await github.issues.addLabels(context.repo({number: issueNum, labels: ['pr-available']}))
+          let promises = []
+          issueNum.forEach(ele => {
+            promises.push(github.issues.removeLabel(context.repo({ number: ele, name: 'pr-rejected' })))
+          })
+          await Promise.all(promises)
+        } catch (e) { }
+
+        // Add label to the issues.
+        let promises = []
+        issueNum.forEach(ele => {
+          promises.push(github.issues.addLabels(context.repo({ number: ele, labels: ['pr-available'] })))
+        })
+        await Promise.all(promises)
+
       }
+
       else if (state === 'closed') {
         // Remove pr-open label if it exists.
         try {
-          await github.issues.removeLabel(context.repo({number: issueNum, name: 'pr-available'}))
-        } catch (e) {}
+          let promises = []
+          issueNum.forEach(ele => {
+            promises.push(github.issues.removeLabel(context.repo({ number: ele, name: 'pr-available' })))
+          })
+          await Promise.all(promises)
+        } catch (e) { }
 
         if (merged) {
           // Remove pr-rejected label if it exists.
           try {
-            await github.issues.removeLabel(context.repo({number: issueNum, name: 'pr-rejected'}))
-          } catch (e) {}
-
-          await github.issues.addLabels(context.repo({number: issueNum, labels: ['pr-merged']}))
+            let promises = []
+            issueNum.forEach(ele => {
+              promises.push(github.issues.removeLabel(context.repo({ number: ele, name: 'pr-rejected' })))
+            })
+            await Promise.all(promises)
+          } catch (e) { }
+          // Add pr-merged label to issues
+          let promises = []
+          issueNum.forEach(ele => {
+            promises.push(github.issues.addLabels(context.repo({ number: ele, labels: ['pr-merged'] })))
+          })
+          await Promise.all(promises)
         }
         else {
-          await github.issues.addLabels(context.repo({number: issueNum, labels: ['pr-rejected']}))
+          let promises = []
+          issueNum.forEach(ele => {
+            promises.push(github.issues.addLabels(context.repo({ number: ele, labels: ['pr-rejected'] })))
+          })
+          await Promise.all(promises)
         }
       }
     }
   }
 
-  function parse (body) {
-    const re = /\B#\d+\b/
-    const found = body.match(re)
-    if (found) {
-      return parseInt(found[0].slice(1), 10)
-    }
+  function parser (body) {
+    const parse_result = parse(body)
+    let issues = []
+    parse_result.actions.forEach(obj => issues.push(parseInt(obj.issue)))
+    return issues
   }
 }

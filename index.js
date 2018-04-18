143d993b14192360491e8c25c8ceaa9d99a44f00
Rohit Sarkar <rohitsarkar5398@gmail.com>
2018-04-18 10:41:58 +0530

    Added support for a configuration file

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const parse = issueParser('github')
 module.exports = (robot) => {
   // TODO: Comment on issue along with adding label on pull_request events.
   // TODO: pull_request.reopened?
+  const default_config = {
+    opened: 'pr-available',
+    merged: 'pr-merged',
+    rejected: 'pr-rejected'
+  }
+
   robot.on(['pull_request.opened',
     'pull_request.edited',
     'pull_request.closed'], pullHandler)
@@ -12,12 +18,14 @@ module.exports = (robot) => {
   async function pullHandler (context) {
     const { github, payload } = context
     const { merged, state, body } = payload.pull_request
+    const userConfig = await context.config('config.yml')
+    const config = userConfig && userConfig.helpr ? {...default_config, ...userConfig.helpr} : default_config
     // Create the labels. TODO : Add a config file for labels and label colors.
     try {
       await Promise.all([
-        github.issues.createLabel(context.repo({ name: 'pr-available', color: '1381ef' })),
-        github.issues.createLabel(context.repo({ name: 'pr-merged', color: '1381ef' })),
-        github.issues.createLabel(context.repo({ name: 'pr-rejected', color: '1381ef' }))
+        github.issues.createLabel(context.repo({ name: config.opened, color: '363396' })),
+        github.issues.createLabel(context.repo({ name: config.merged, color: '363396' })),
+        github.issues.createLabel(context.repo({ name: config.rejected, color: '363396' }))
       ])
     } catch (e) { }
 
@@ -31,7 +39,7 @@ module.exports = (robot) => {
         try {
           let promises = []
           issueNum.forEach(ele => {
-            promises.push(github.issues.removeLabel(context.repo({ number: ele, name: 'pr-rejected' })))
+            promises.push(github.issues.removeLabel(context.repo({ number: ele, name: config.rejected })))
           })
           await Promise.all(promises)
         } catch (e) { }
@@ -39,7 +47,7 @@ module.exports = (robot) => {
         // Add label to the issues.
         let promises = []
         issueNum.forEach(ele => {
-          promises.push(github.issues.addLabels(context.repo({ number: ele, labels: ['pr-available'] })))
+          promises.push(github.issues.addLabels(context.repo({ number: ele, labels: [config.opened] })))
         })
         await Promise.all(promises)
 
@@ -50,7 +58,7 @@ module.exports = (robot) => {
         try {
           let promises = []
           issueNum.forEach(ele => {
-            promises.push(github.issues.removeLabel(context.repo({ number: ele, name: 'pr-available' })))
+            promises.push(github.issues.removeLabel(context.repo({ number: ele, name: config.opened })))
           })
           await Promise.all(promises)
         } catch (e) { }
@@ -60,21 +68,21 @@ module.exports = (robot) => {
           try {
             let promises = []
             issueNum.forEach(ele => {
-              promises.push(github.issues.removeLabel(context.repo({ number: ele, name: 'pr-rejected' })))
+              promises.push(github.issues.removeLabel(context.repo({ number: ele, name: config.rejected })))
             })
             await Promise.all(promises)
           } catch (e) { }
           // Add pr-merged label to issues
           let promises = []
           issueNum.forEach(ele => {
-            promises.push(github.issues.addLabels(context.repo({ number: ele, labels: ['pr-merged'] })))
+            promises.push(github.issues.addLabels(context.repo({ number: ele, labels: [config.merged] })))
           })
           await Promise.all(promises)
         }
         else {
           let promises = []
           issueNum.forEach(ele => {
-            promises.push(github.issues.addLabels(context.repo({ number: ele, labels: ['pr-rejected'] })))
+            promises.push(github.issues.addLabels(context.repo({ number: ele, labels: [config.rejected] })))
           })
           await Promise.all(promises)
         }
